968e362722f4fbf493b4d75323617ad4340e5e50
Lomemoraine <lorrainechepkemoi4@gmail.com>
2022-06-05 10:04:40 +0300

    structure models

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class Profile(models.Model):
+    image = CloudinaryField('images/', default="", blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=300, default="Share more about yourself", blank=True)
+
+def __str__(self):
+        return f'{self.user.username} Profile'
+
+class Post(models.Model):
+    post_image = CloudinaryField('images/', default="", blank=True, null=True)
+    image_name = models.CharField(max_length=50,blank=True)
+    image_caption = models.TextField(max_length=500,blank=True)
+    image_owner = models.ForeignKey(Profile,null=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image_name
+       
--- insta/views.py
+++ insta/views.py
@@ -1,12 +1,14 @@
 from email import message
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib import messages
 
 # Create your views here.
+@login_required(login_url='/login')
 def home(request):
     return render(request, 'general/index.html')
 
