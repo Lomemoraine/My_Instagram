3675c2d31711c68ee124a0a38f51b538bc2e3982
Lomemoraine <lorrainechepkemoi4@gmail.com>
2022-06-07 03:18:59 +0300

    save for backup

--- insta/urls.py
+++ insta/urls.py
@@ -4,16 +4,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('home', views.home, name='home'),
-    path('', views.sign_up, name='login'),
-    path('sign-up', views.sign_up, name='sign-up'),
-    path('profile/', views.profile, name='profile'),
-    path('create-post', views.create_post, name='create_post'),
-    path('author_profile/', views.author_profile, name='author_profile'),
-    path('edit_profile/', views.editProfile, name='edit_profile'),
-    path('search/', views.search_profile, name='search'),
-    path('profile/<str:username>/',views.profile,name='profile'),
-    path('like-post/', views.like_post, name='like'),
+    # path('home', views.home, name='home'),
+    # # path('', views.sign_up, name='login'),
+    # # path('sign-up', views.sign_up, name='sign-up'),
+    # # path('profile/', views.profile, name='profile'),
+    # # path('create-post', views.create_post, name='create_post'),
+    # # path('author_profile/<str:username>', views.author_profile, name='author_profile'),
+    # # path('edit_profile/', views.editProfile, name='edit_profile'),
+    # # path('search/', views.search_profile, name='search'),
+    # # path('profile/<str:username>/',views.profile,name='profile'),
+    # # path('like-post/', views.like_post, name='like'),
     
     
 ]
